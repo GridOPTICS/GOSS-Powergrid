59a46e61303236f22504f45adbc6156f6329f8f8
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-01-28 08:39:57 -0800

    Added ordered appending of log messages for results.

--- powergrid-server/src/main/java/pnnl/goss/powergrid/parsers/ResultLog.groovy
+++ powergrid-server/src/main/java/pnnl/goss/powergrid/parsers/ResultLog.groovy
@@ -4,22 +4,48 @@ import java.util.List;
 
 import pnnl.goss.powergrid.PowergridCreationReport;
 
-class ParserResultLog implements PowergridCreationReport{
+class ResultLog {
+    def debugEnabled = true
 
     def warnings = []
     def errors = []
+    def debug = []
+    def ordered = []
+    boolean successful = false
 
-    def warn(String warning) {
-        warnings << sprintf("WARN: %s", warning)
+    def debug(String message) {
+        debug << message
+        ordered << [type: 'debug', message: message]
+    }
+    def warn(String message) {
+        warnings << sprintf("WARN: %s", message)
+        ordered << [type: 'warn', message: message]
+    }
+
+    def error(String message){
+        errors << message
+        ordered << [type: 'error', message: error]
     }
 
     def error(String section, int linenum, String error){
         errors << sprintf("%s: %5d %s", [section, linenum, error])
+        ordered << [type: 'error', message: error]
     }
 
-    @Override
-    public List<String> getReport() {
-        return warnings + errors;
+    public List<String> getLog() {
+        def items = []
+        if (debugEnabled){
+            ordered.each{ msg ->
+                items << sprintf("%6s: %s", msg.type, msg.message)
+            }
+        }
+        else{
+            ordered.each{ msg ->
+                if (msg != 'debug'){
+                    items << sprintf("%6s: %s", msg.type, msg.message)
+                }
+            }
+        }
+        items
     }
-
 }
