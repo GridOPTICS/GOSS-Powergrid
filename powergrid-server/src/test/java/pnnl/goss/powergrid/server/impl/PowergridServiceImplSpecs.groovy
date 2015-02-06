7d592265ccd962d0dff4f73de1abcd7a8702dc1e
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-02-06 15:18:23 -0800

    Started using 118 model for tests.

--- powergrid-server/src/test/java/pnnl/goss/powergrid/server/impl/PowergridServiceImplSpecs.groovy
+++ powergrid-server/src/test/java/pnnl/goss/powergrid/server/impl/PowergridServiceImplSpecs.groovy
@@ -1,18 +1,26 @@
 package pnnl.goss.powergrid.server.impl
 
+import java.beans.PersistenceDelegate;
+
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence;
+
 import pnnl.goss.powergrid.PowergridCreationReport;
 import pnnl.goss.powergrid.server.impl.PowergridServiceImpl
 import spock.lang.Specification
 
 class PowergridServiceImplSpecs extends Specification {
     PowergridServiceImpl pgService
+    EntityManagerFactory factory
 
     def setup(){
+        factory = Persistence.createEntityManagerFactory("mysqlPU")
         pgService = new PowergridServiceImpl()
+        pgService.setEntityManagerFactory(factory)
     }
 
     def "can create powergrid from pti23 IEEE14 bus file"(){
-        File ieee14File = new File("src/test/resources/IEEE14.raw")
+        File ieee14File = new File("src/test/resources/118.raw")
         expect:
         ieee14File.exists()
 
@@ -22,22 +30,23 @@ class PowergridServiceImplSpecs extends Specification {
 
         then:
         assert report.wasSuccessful()
+        // Retrieve all of the powergrids so we can find the one we want.
+        def powergrids = pgService.getPowergrids()
+        assert powergrids.size() == 1  // There is only one...so grab it.
+        def pg = powergrids.get(0)
+        def pgModel = pgService.getPowergridModel(pg.mrid)  // now get our model.
 
+        // TODO Added test for other powergrid properties.
+        assert pgModel.powergrid.name == pg.name
+        assert pgModel.powergrid.mrid == pg.mrid
+		
 
     }
 
-//    def "upload psse23 file"(){
-//        def file = new File("src/test/resources/IEEE14.raw")
-//        when: "called upload"
-//        def results = pgService.createModelFromFile(file)
-//        then:
-//        assert results != null
-//        assert pgService.model != null
-//        assert pgService.model.buses.size() == 14
-//
-//    }
 
     def cleanup(){
+        factory.close()
+        factory = null
         pgService = null
     }
 
