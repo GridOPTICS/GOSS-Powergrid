2b0d5462730b002cb2d6031d0b995813c93d48a3
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-02-06 20:40:59 -0800

    Updated powergridmodel to include all subtypes that we want to represent.

--- powergrid-common/src/main/java/pnnl/goss/powergrid/models/PowergridModel.java
+++ powergrid-common/src/main/java/pnnl/goss/powergrid/models/PowergridModel.java
@@ -10,8 +10,16 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import pnnl.goss.powergrid.datamodel.Area;
+import pnnl.goss.powergrid.datamodel.Branch;
 import pnnl.goss.powergrid.datamodel.Bus;
+import pnnl.goss.powergrid.datamodel.Line;
+import pnnl.goss.powergrid.datamodel.Load;
+import pnnl.goss.powergrid.datamodel.Machine;
 import pnnl.goss.powergrid.datamodel.Powergrid;
+import pnnl.goss.powergrid.datamodel.SwitchedShunt;
+import pnnl.goss.powergrid.datamodel.Transformer;
+import pnnl.goss.powergrid.datamodel.Zone;
 
 @XmlRootElement(name="PowergridModel")
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -33,12 +41,111 @@ public class PowergridModel implements Serializable{
     @XmlElement(name="Bus" )
     List<Bus> buses = null;
 
+    @XmlElementWrapper(name="Machines")
+    @XmlElement(name="Machine" )
+    List<Machine> machines = null;
+
+    //List<Owner> owners = null;
+    @XmlElementWrapper(name="Areas")
+    @XmlElement(name="Area" )
+    List<Area> areas = null;
+
+    @XmlElementWrapper(name="Zones")
+    @XmlElement(name="Zone" )
+    List<Zone> zones = null;
+
+    @XmlElementWrapper(name="Transformers")
+    @XmlElement(name="Transformer" )
+    List<Transformer> transformers = null;
+
+    @XmlElementWrapper(name="Branches")
+    @XmlElement(name="Branch" )
+    List<Branch> branches = null;
+
+    @XmlElementWrapper(name="Lines")
+    @XmlElement(name="Line" )
+    List<Line> lines = null;
+
+    @XmlElementWrapper(name="Loads")
+    @XmlElement(name="Load" )
+    List<Load> loads = null;
+
+    @XmlElementWrapper(name="SwitchedShunts")
+    @XmlElement(name="SwitchedShunt" )
+    List<SwitchedShunt> switchedShunts = null;
+
+
+    public List<SwitchedShunt> getSwitchedShunts() {
+        return switchedShunts;
+    }
+
+    public void setSwitchedShunts(List<SwitchedShunt> switchedShunts) {
+        this.switchedShunts = switchedShunts;
+    }
+
     public List<Bus> getBuses(){
         return buses;
     }
 
     public void setBuses(List<Bus> buses){
         this.buses = buses;
+    }
+
+    public void setMachines(List<Machine> machineList) {
+        machines = machineList;
+    }
+
+    public List<Machine> getMachines(){
+        return machines;
+    }
+
+    public List<Area> getAreas() {
+        return areas;
+    }
+
+    public void setAreas(List<Area> areas) {
+        this.areas = areas;
+    }
+
+    public List<Zone> getZones() {
+        return zones;
+    }
+
+    public void setZones(List<Zone> zones) {
+        this.zones = zones;
+    }
+
+    public List<Transformer> getTransformers() {
+        return transformers;
+    }
+
+    public void setTransformers(List<Transformer> transformers) {
+        this.transformers = transformers;
+    }
+
+    public void setLines(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public List<Line> getLines(){
+        return this.lines;
+    }
+
+
+    public List<Branch> getBranches() {
+        return branches;
+    }
+
+    public void setBranches(List<Branch> branches) {
+        this.branches = branches;
+    }
+
+    public List<Load> getLoads() {
+        return loads;
+    }
+
+    public void setLoads(List<Load> loads) {
+        this.loads = loads;
     }
 
     public Powergrid getPowergrid() {
@@ -73,5 +180,7 @@ public class PowergridModel implements Serializable{
 //
 //        return errors
     }
+
+
 
 }
