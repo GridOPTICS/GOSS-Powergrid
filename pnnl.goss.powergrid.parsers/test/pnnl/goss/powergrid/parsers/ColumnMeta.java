8f521a757d0b5cd9e44f25bccb090b7b307a198f
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-04-08 20:38:29 -0700

    Added copy constructor for MetaData classes.

--- pnnl.goss.powergrid.parsers/test/pnnl/goss/powergrid/parsers/ColumnMeta.java
+++ pnnl.goss.powergrid.parsers/test/pnnl/goss/powergrid/parsers/ColumnMeta.java
@@ -1,11 +1,25 @@
 package pnnl.goss.powergrid.parsers;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import pnnl.goss.powergrid.parser.api.InvalidDataException;
+
 public class ColumnMeta {
 	
 	private String field;
 	private String outputFormat;
 	private String dataType;
 	private String description;
+	
+	public ColumnMeta() {}
+	public ColumnMeta(ColumnMeta meta){
+		// Copy ok because Strings are immutable
+		field = meta.field;
+		outputFormat = meta.outputFormat;
+		dataType = meta.dataType;
+		description = meta.description;
+	}
 	
 	public String getField() {
 		return field;
@@ -32,6 +46,25 @@ public class ColumnMeta {
 		this.description = description;
 	}
 	
-	
-
+	/**
+	 * Returns the class associated with Integer, Double, or String
+	 * based upon what is in the dataType field.
+	 * 
+	 * @return
+	 * @throws InvalidDataException
+	 */
+	@SuppressWarnings("rawtypes")
+	public Class getPrimativeType() throws InvalidDataException{
+		if (dataType.toLowerCase().equals("int")){
+			return Integer.class;
+		}
+		else if(dataType.toLowerCase().equals("double")){
+			return Double.class;
+		}
+		else if(dataType.toLowerCase().equals("string")){
+			return Double.class;
+		}
+		
+		throw new InvalidDataException(Arrays.asList(new String[]{"invalid dataType!"}));
+	}
 }
--- pnnl.goss.powergrid.parsers/test/pnnl/goss/powergrid/parsers/ColumnMetaGroup.java
+++ pnnl.goss.powergrid.parsers/test/pnnl/goss/powergrid/parsers/ColumnMetaGroup.java
@@ -1,8 +1,19 @@
 package pnnl.goss.powergrid.parsers;
 
+
 public class ColumnMetaGroup {
 	private String name;
 	private ColumnMeta[] columns;
+	
+	public ColumnMetaGroup(){	}
+	
+	public ColumnMetaGroup(ColumnMetaGroup meta){
+		// ok because strings are immutable.
+		name = meta.name;
+		
+		columns = new ColumnMeta[meta.columns.length];
+		System.arraycopy(meta.columns, 0, columns, 0, meta.columns.length);
+	}
 	
 	public String getName() {
 		return name;
