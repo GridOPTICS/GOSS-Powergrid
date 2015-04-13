fcd361607519e9b829ae99709565a4ce685957fd
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-04-12 17:20:37 -0700

    Added a parserdefinition class that can be used to define what capabilities that a parser has

---
+++ pnnl.goss.powergrid.parsers/src/pnnl/goss/powergrid/parser/api/ParserDefinition.java
@@ -0,0 +1,31 @@
+package pnnl.goss.powergrid.parser.api;
+
+public class ParserDefinition {
+	
+	private boolean isLineSeperated;
+	private String description;
+	private String name;
+
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isLineSeperated() {
+		return isLineSeperated;
+	}
+	public void setLineSeperated(boolean isLineSeperated) {
+		this.isLineSeperated = isLineSeperated;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
