9e56f84cea3ab978274fea8dd075f64098c86936
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-06-06 11:10:35 -0700

    Updated powergrid createion and datasoureces.

--- pnnl.goss.powergrid/src/pnnl/goss/powergrid/handlers/CreatePowergridHandler.java
+++ pnnl.goss.powergrid/src/pnnl/goss/powergrid/handlers/CreatePowergridHandler.java
@@ -5,8 +5,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.sql.DataSource;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.felix.dm.annotation.api.Component;
+import org.apache.felix.dm.annotation.api.ServiceDependency;
 
 import pnnl.goss.core.DataError;
 import pnnl.goss.core.DataResponse;
@@ -14,6 +17,9 @@ import pnnl.goss.core.Request;
 import pnnl.goss.core.Response;
 import pnnl.goss.core.security.AuthorizationHandler;
 import pnnl.goss.core.security.AuthorizeAll;
+import pnnl.goss.core.server.DataSourceObject;
+import pnnl.goss.core.server.DataSourcePooledJdbc;
+import pnnl.goss.core.server.DataSourceRegistry;
 import pnnl.goss.core.server.RequestHandler;
 import pnnl.goss.powergrid.parser.api.InvalidDataException;
 import pnnl.goss.powergrid.parser.api.ParserService;
@@ -26,9 +32,14 @@ import pnnl.goss.powergrid.server.datasources.PowergridDataSource;
 @Component
 public class CreatePowergridHandler implements RequestHandler {
 	
+	
+	@ServiceDependency
+	private volatile DataSourceRegistry datasourceRegistry;
+	
 	//@ServiceDependency
 	private volatile ParserService parserService = new ParserServiceImpl();
-	private PowergridDataSource datasource = new PowergridDataSource("powergrid", "manager", "jdbc:mysql://localhost:3306/dev_powergrids");
+	
+	//private PowergridDataSource datasource = new PowergridDataSource("powergrid", "manager", "jdbc:mysql://localhost:3306/dev_powergrids");
 
 	@Override
 	public Map<Class<? extends Request>, Class<? extends AuthorizationHandler>> getHandles() {
@@ -64,7 +75,10 @@ public class CreatePowergridHandler implements RequestHandler {
 	
 	
 	private String saveData(Map<String, List<PropertyGroup>> parsedData){
-		PowergridDaoMySql mydata = new PowergridDaoMySql(datasource.getDatasource());
+		//DataSourcePooledJdbc obj = (DataSourcePooledJdbc)datasourceRegistry.get("powergrid");
+		PowergridDaoMySql mydata = new PowergridDaoMySql((DataSource) datasourceRegistry.get("powergrid"));
+		
+		//PowergridDaoMySql mydata = new PowergridDaoMySql((DataSource) obj);
 		return mydata.createPowergrid("new powergrid", parsedData);
 	}
 
--- pnnl.goss.powergrid/src/pnnl/goss/powergrid/server/datasources/PowergridDataSource.java
+++ pnnl.goss.powergrid/src/pnnl/goss/powergrid/server/datasources/PowergridDataSource.java
@@ -4,94 +4,61 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
+import javax.sql.ConnectionPoolDataSource;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.dbcp.BasicDataSourceFactory;
+import org.apache.felix.dm.annotation.api.Component;
+import org.apache.felix.dm.annotation.api.ServiceDependency;
+import org.apache.felix.dm.annotation.api.Start;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import pnnl.goss.core.server.DataSourceBuilder;
 import pnnl.goss.core.server.DataSourceObject;
 import pnnl.goss.core.server.DataSourcePooledJdbc;
+import pnnl.goss.core.server.DataSourceRegistry;
 import pnnl.goss.core.server.DataSourceType;
 
+@Component
 public class PowergridDataSource implements DataSourcePooledJdbc, DataSourceObject {
 
+	public static final String DS_NAME = "goss.powergrids";
 	private static final Logger log = LoggerFactory.getLogger(PowergridDataSource.class);
 	private DataSource datasource;
 	
-	public PowergridDataSource(String username, String password, String url){
-		try {
-			createMysql("dont.worry", url, username, password);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	// Eventually to hold more than one connection
+	// private Map<String, ConnectionPoolDataSource> pooledMap = new ConcurrentHashMap<>();
+	private ConnectionPoolDataSource pooledDataSource;
 	
-	public DataSource getDatasource(){
-		return datasource;
-	}
+	@ServiceDependency
+	private DataSourceBuilder datasourceBuilder;
+	
+	
+	@Start
+	public void start(){
+		// Need to read properties from a file and then create the data
+		Properties properties = new Properties();
+		
+		properties.put(DataSourceBuilder.DATASOURCE_NAME, DS_NAME);
+		properties.put(DataSourceBuilder.DATASOURCE_USER, "manager");
+		properties.put(DataSourceBuilder.DATASOURCE_URL, "jdbc:mysql://localhost:3306/dev_powergrids");
 			
-	private void createMysql(String dsName, String url, String username, String password) throws Exception{
-		create(dsName, url, username, password, "com.mysql.jdbc.Driver");
-	}
-
-	private void create(String dsName, String url, String username, String password,
-			String driver) throws Exception {
-		
-		Properties propertiesForDataSource = new Properties();
-		propertiesForDataSource.setProperty("username", username);
-		propertiesForDataSource.setProperty("password", password);
-		propertiesForDataSource.setProperty("url", url);
-		propertiesForDataSource.setProperty("driverClassName", driver);
-		
-		create(dsName, propertiesForDataSource);
 	}
 	
-	private void create(String dsName, Properties properties) throws Exception {
-		
-		List<String> checkItems = Arrays.asList(new String[]{"username", "password", "url", "driverClassName"});
-		
-		for (String item: checkItems){
-			if(properties.containsKey(item)){
-				String value = properties.getProperty(item);
-				if (value == null || value.isEmpty()){
-					throw new IllegalArgumentException(item + " was specified incorrectly!");
-				}
-			}
-			else{
-				throw new IllegalArgumentException(item+" must be specified!");
-			}
-		}
-		
-		if (!properties.containsKey("maxOpenPreparedStatements")){
-			properties.setProperty("maxOpenPreparedStatements", "10");
-		}
-		
-		log.debug("Creating BasicDataSource\n\tURI:"+properties.getProperty("url")+"\n\tUser:\n\t"+properties.getProperty("username"));
-		
-		Class.forName(properties.getProperty("driverClassName"));
-		
-		datasource = BasicDataSourceFactory.createDataSource(properties);
-		
-		
-		//registry.add(dsName, new DataSourceObjectImpl(dsName, DataSourceType.DS_TYPE_JDBC, ds));		
-	}
-
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return DS_NAME;
 	}
 
 	@Override
 	public DataSourceType getDataSourceType() {
-		// TODO Auto-generated method stub
-		return null;
+		return DataSourceType.DS_TYPE_JDBC;
 	}
 
 	@Override
