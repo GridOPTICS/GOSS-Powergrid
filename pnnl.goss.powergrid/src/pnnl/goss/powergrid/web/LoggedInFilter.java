e48f46f6031f02a1dc7c458cd12cc1d855b1944e
Craig A. <craig.allwardt@pnnl.gov>
2015-06-27 22:27:56 -0700

    Added better handling of token for both GET and POST requests.  Updated comments for class and other functions.

--- pnnl.goss.powergrid/src/pnnl/goss/powergrid/web/LoggedInFilter.java
+++ pnnl.goss.powergrid/src/pnnl/goss/powergrid/web/LoggedInFilter.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -20,13 +19,25 @@ import org.apache.felix.dm.annotation.api.ServiceDependency;
 import org.apache.felix.dm.annotation.api.Start;
 import org.apache.felix.dm.annotation.api.Stop;
 import org.apache.felix.http.api.ExtHttpService;
-import org.osgi.service.http.HttpService;
+
+import pnnl.goss.core.server.TokenIdentifierMap;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import pnnl.goss.core.server.TokenIdentifierMap;
-
+/**
+ * This filter tests that a user has logged in before allowing
+ * access to the requested resource.  It does this by using a
+ * {@link TokenIdentifierMap} based service that will check the
+ * ip address and the pressence of a valid token.
+ *
+ * If a valid token is present then the request will modified to
+ * include an "identifier" parameter that can be used in a web request
+ * to authenticate a user's permissions.
+ *
+ * @author Craig Allwardt
+ *
+ */
 @Component
 public class LoggedInFilter implements Filter
 {
@@ -61,58 +72,107 @@ public class LoggedInFilter implements Filter
         doLog("Init with config [" + config + "]");
     }
 
+    /**
+     * Retrieves a token from the passed request.  The token could be
+     * in a header if a GET request or in either the header or body
+     * of the request if a POST request.
+     *
+     * @param request
+     * @return The token or a null string.
+     */
+    private String getTokenIfPresent(HttpServletRequest request){
+
+    	String token = request.getHeader("AuthToken");
+
+    	// Not available through the header
+    	if (token == null || token.isEmpty()){
+
+    		// If POST request then check the content of the body for an
+    		// AuthToken element
+    		if (request.getMethod().equalsIgnoreCase("POST")){
+    			StringBuilder body = new StringBuilder();
+        		char[] charBuffer = new char[128];
+        		InputStream inputStream;
+				try {
+					inputStream = request.getInputStream();
+					int bytesRead = -1;
+	        		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+	        		while ((bytesRead = reader.read(charBuffer)) > 0) {
+	        			body.append(charBuffer, 0, bytesRead);
+	        		}
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				if (!body.toString().isEmpty()){
+
+	        		try {
+
+	        			Gson gson = new Gson();
+
+	        			JsonObject json = gson.fromJson(body.toString(), JsonObject.class);
+	        			token = json.get("AuthToken").getAsString();
+
+	        			// Return a null for an empty token string.
+	        			if (token.isEmpty()){
+	        				token = null;
+	        			}
+
+
+	        		}catch (Exception e){
+	        			e.printStackTrace();
+	        		}
+				}
+    		}
+    	}
+
+    	return token;
+    }
+
+    /*
+     * This function is designed to validate that a user has been logged into
+     * the system and made a request within a period of time.  The time is
+     * not determined in this class but in the {@link TokenIdentifiedMap} service.
+     * In addition the token and ip address will be checked to make sure the
+     * origin of the request is from the same ip.
+     *
+     * If the request is a GET request then the header AuthToken must be present
+     * with a validated token.  If a POST request then the AuthToken can either
+     * be present in the header or in a json body element.
+     *
+     * If the AuthToken is valid then an 'identifier' parameter will be set on the
+     * request before it is sent to the next filter.
+     *
+     * (non-Javadoc)
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
         throws IOException, ServletException
     {
     	HttpServletRequest httpReq = (HttpServletRequest) req;
+    	String authToken = getTokenIfPresent(httpReq);
     	String ip = httpReq.getRemoteAddr();
-//    	Enumeration<String> en = httpReq.getHeaderNames();
-//    	while(en.hasMoreElements()){
-//    		System.out.println("en is: "+en.nextElement());
-//    	}
-    	String token = httpReq.getHeader("AuthToken");
+    	String identifier = null;
+    	boolean identifierSet = false;
 
-    	if (token != null){
-    		String user = idMap.getIdentifier(ip, token);
-    		if (user != null){
-    			req.setAttribute("subject", user);
-    		}
-    		else{
-    			PrintWriter out = res.getWriter();
-    			out.write("Invalid Login Attempt");
-    			out.close();
-    			return;
+    	if (authToken != null){
+    		identifier = idMap.getIdentifier(ip, authToken);
+    		if (identifier != null && !identifier.isEmpty()){
+    			req.setAttribute("identifier", identifier);
+    			identifierSet = true;
     		}
     	}
-    	else{
-    		StringBuilder body = new StringBuilder();
-    		char[] charBuffer = new char[128];
-    		InputStream inputStream = httpReq.getInputStream();
-    		int bytesRead = -1;
-    		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-    		while ((bytesRead = reader.read(charBuffer)) > 0) {
-    			body.append(charBuffer, 0, bytesRead);
-    		}
 
-    		Gson gson = new Gson();
+    	if (!identifierSet){
+    		PrintWriter out = res.getWriter();
+			out.write("{\"error\":\"Invalid Authentication Token\"}");
+			out.close();
+			return;
+    	}
 
-    		JsonObject json = gson.fromJson(body.toString(), JsonObject.class);
-    		token = json.get("authenticated").getAsString();
-
-    		String user = idMap.getIdentifier(ip, token);
-    		if (user != null){
-    			req.setAttribute("subject", user);
-    		}
-    		else{
-    			PrintWriter out = res.getWriter();
-    			out.write("Invalid Login Attempt");
-    			out.close();
-    			return;
-    		}
-		}
-
-        doLog("Filter request [" + req + "]");
+        System.out.println("Identifier set: "+identifier);
         chain.doFilter(req, res);
     }
 
