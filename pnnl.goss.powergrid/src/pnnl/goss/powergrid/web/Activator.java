7d6d7a952c27fc95f6a56f01f907fe44935f74fe
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-09-11 17:34:34 -0700

    Use activator for registering filters using whiteboard.

--- pnnl.goss.powergrid/src/pnnl/goss/powergrid/web/Activator.java
+++ pnnl.goss.powergrid/src/pnnl/goss/powergrid/web/Activator.java
@@ -1,19 +1,38 @@
 package pnnl.goss.powergrid.web;
 
+
+import java.util.Hashtable;
+
+import javax.servlet.Filter;
+
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.osgi.framework.BundleContext;
 
 import pnnl.goss.core.server.RequestHandlerRegistry;
+import pnnl.goss.core.server.TokenIdentifierMap;
 import pnnl.goss.powergrid.api.PowergridService;
 import pnnl.goss.powergrid.parser.api.RequestSubjectService;
 import pnnl.goss.sytheticdata.api.SimulatorService;
 
 public class Activator extends DependencyActivatorBase {
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public void init(BundleContext context, DependencyManager manager)
 			throws Exception {
+		
+		Hashtable loggedProps = new Hashtable();
+		loggedProps.put("pattern", "/powergrid/api/.*");
+		loggedProps.put("contextId", "GossContext");
+			
+		// Login filter
+		manager.add(createComponent()
+				.setInterface(Filter.class.getName(), loggedProps)
+				.setImplementation(PowergridLoggedInFilter.class)
+				.add(createServiceDependency()
+						.setService(TokenIdentifierMap.class)));
+		
 		manager.add(createComponent()
 				.setInterface(Object.class.getName(), null)
 				.setImplementation(PowergridWebService.class)
