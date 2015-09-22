2835501360fc42cc669a1619cd0bc46e0d6a8377
Craig <craig.allwardt@pnnl.gov>
2015-09-21 23:27:33 -0700

    Cleanup code.

--- pnnl.goss.powergrid/src/pnnl/goss/powergrid/server/PowergridDataSources.java
+++ pnnl.goss.powergrid/src/pnnl/goss/powergrid/server/PowergridDataSources.java
@@ -27,41 +27,41 @@ public class PowergridDataSources implements PowergridDataSourceEntries {
 	public static final String DS_NAME = "goss.powergrids";
 	private static final Logger log = LoggerFactory.getLogger(PowergridDataSources.class);
 //	private DataSource datasource;
-//	
+//
 //	// Eventually to hold more than one connection
 //	// private Map<String, ConnectionPoolDataSource> pooledMap = new ConcurrentHashMap<>();
 //	private ConnectionPoolDataSource pooledDataSource;
-	
+
 	@ServiceDependency
 	private DataSourceBuilder datasourceBuilder;
-	
+
 	@ServiceDependency
 	private DataSourceRegistry datasourceRegistry;
-	
+
 	// These are the datasources that this module has registered.
 	private List<String> registeredDatasources = new ArrayList<>();
-	
+
 	public List<String> getRegisteredDatasources(){
 		return registeredDatasources;
 	}
-	
-	
+
+
 	@Start
 	public void start(){
 		log.debug("Starting "+this.getClass().getName());
 		// Need to read properties from a file and then create the data
 		Properties properties = new Properties();
-		
+
 		properties.put(DataSourceBuilder.DATASOURCE_NAME, DS_NAME+".north");
 		properties.put(DataSourceBuilder.DATASOURCE_USER, "powergrid");
 		properties.put(DataSourceBuilder.DATASOURCE_PASSWORD, "manager");
 		properties.put(DataSourceBuilder.DATASOURCE_URL, "jdbc:mysql://localhost:3306/dev_powergrids");
 		properties.put("driverClassName", "com.mysql.jdbc.Driver");
-		
+
 		try {
 			datasourceBuilder.create(DS_NAME+".north", properties);
 			registeredDatasources.add(DS_NAME+".north");
-			
+
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -70,14 +70,14 @@ public class PowergridDataSources implements PowergridDataSourceEntries {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Stop
 	public void stop(){
 		log.debug("Stopping "+this.getClass().getName());
 		for(String s: registeredDatasources){
 			datasourceRegistry.remove(s);
 		}
-		registeredDatasources.clear();		
+		registeredDatasources.clear();
 	}
 
 	@Override
@@ -87,7 +87,7 @@ public class PowergridDataSources implements PowergridDataSourceEntries {
 
 	@Override
 	public Connection getConnectionByPowergrid(String mrid) {
-		String key = this.registeredDatasources.get(0); 
+		String key = this.registeredDatasources.get(0);
 		Connection conn = null;
 		try {
 			conn = ((DataSourcePooledJdbc) datasourceRegistry.get(key)).getConnection();
@@ -114,7 +114,7 @@ public class PowergridDataSources implements PowergridDataSourceEntries {
 	}
 
 
-	
+
 //	@Override
 //	public String getName() {
 //		return DS_NAME;
@@ -124,9 +124,9 @@ public class PowergridDataSources implements PowergridDataSourceEntries {
 //	public DataSourceType getDataSourceType() {
 //		return DataSourceType.DS_TYPE_JDBC;
 //	}
-	
-	
-	
+
+
+
 //	public PowergridDataSource(String username, String password, String url){
 //		try {
 //			createMysql("dont.worry", url, username, password);
@@ -134,27 +134,27 @@ public class PowergridDataSources implements PowergridDataSourceEntries {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-//	}			
+//	}
 //	private void createMysql(String dsName, String url, String username, String password) throws Exception{
 //		create(dsName, url, username, password, "com.mysql.jdbc.Driver");
 //	}
 //
 //	private void create(String dsName, String url, String username, String password,
 //			String driver) throws Exception {
-//		
+//
 //		Properties propertiesForDataSource = new Properties();
 //		propertiesForDataSource.setProperty("username", username);
 //		propertiesForDataSource.setProperty("password", password);
 //		propertiesForDataSource.setProperty("url", url);
 //		propertiesForDataSource.setProperty("driverClassName", driver);
-//		
+//
 //		create(dsName, propertiesForDataSource);
 //	}
-//	
+//
 //	private void create(String dsName, Properties properties) throws Exception {
-//		
+//
 //		List<String> checkItems = Arrays.asList(new String[]{"username", "password", "url", "driverClassName"});
-//		
+//
 //		for (String item: checkItems){
 //			if(properties.containsKey(item)){
 //				String value = properties.getProperty(item);
@@ -166,19 +166,19 @@ public class PowergridDataSources implements PowergridDataSourceEntries {
 //				throw new IllegalArgumentException(item+" must be specified!");
 //			}
 //		}
-//		
+//
 //		if (!properties.containsKey("maxOpenPreparedStatements")){
 //			properties.setProperty("maxOpenPreparedStatements", "10");
 //		}
-//		
+//
 //		log.debug("Creating BasicDataSource\n\tURI:"+properties.getProperty("url")+"\n\tUser:\n\t"+properties.getProperty("username"));
-//		
+//
 //		Class.forName(properties.getProperty("driverClassName"));
-//		
+//
 //		datasource = BasicDataSourceFactory.createDataSource(properties);
-//		
-//		
-//		//registry.add(dsName, new DataSourceObjectImpl(dsName, DataSourceType.DS_TYPE_JDBC, ds));		
+//
+//
+//		//registry.add(dsName, new DataSourceObjectImpl(dsName, DataSourceType.DS_TYPE_JDBC, ds));
 //	}
 
 
